10503060dbc3f5b8a5cb949e3e7af3a97b300acc
HBCDresden <HBCDresden@users.noreply.github.com>
2020-02-23 15:50:49 +0100

    Refaktoryzacja kodu testow xml

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -1,4 +1,5 @@
 import json
+import xml.etree.cElementTree as ET
 PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
@@ -25,8 +26,13 @@ def get_formatted(msg, imie, format):
 
 
 def format_to_xml(msg, imie):
-    return ('<greetings><name>' + imie + ' ' + '</name><msg>' + msg +
-            '</msg></greetings>')
+    # <greetings>
+    greetings = ET.Element("greetings")
+    # <name>
+    ET.SubElement(greetings, "name").text = imie
+    # <msg>
+    ET.SubElement(greetings, "msg").text = msg
+    return ET.tostring(greetings)
 
 
 def format_to_json(msg, imie):
--- hello_world/test/test_views.py
+++ hello_world/test/test_views.py
@@ -1,8 +1,10 @@
 import unittest
+import xml.etree.cElementTree as ET
 from hello_world import app
 from hello_world.formater import SUPPORTED
 
-
+name = 'Krzysiu'
+msg = 'Hello World!'
 class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
@@ -19,8 +21,11 @@ class FlaskrTestCase(unittest.TestCase):
         self.assertEqual(data_test, rv.data)
 
     def test_message_with_xml_output(self):
-        name = 'Krzysiu'
-        data_test = '<greetings><name>' + name + ' ' + \
-            '</name><msg>Hello World!</msg></greetings>'
+        # <greetings>
+        greetings = ET.Element("greetings")
+        # <name>
+        ET.SubElement(greetings, "name").text = name
+        # <msg>
+        ET.SubElement(greetings, "msg").text = msg
         xml = self.app.get('/?output=xml&name=' + name)
-        self.assertEqual(data_test, xml.data)
+        self.assertEquals(ET.tostring(greetings), xml.data)
